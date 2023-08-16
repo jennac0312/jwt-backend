1d2395d3665a3eb44c285efbfe857ba65d7dd46b
Jenna C <jennac0312@gmail.com>
2023-08-16 15:27:42 -0400

    require middleware in auth.js routes/api

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth') // bring in auth middleware
 
 // @route:   GET api/auth
 // @desc:    Test route
 // @access:  Public
 router.get('/', (req, res) => res.send('Auth Route'));
+
+// @route:   GET api/auth
+// @desc:    Get User data
+// @access:  Public
+router.get('/', async ( req, res ) => {
+
+})
 
 module.exports = router;
