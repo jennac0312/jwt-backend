6d8813ed70e4f99f0d6834f624c58015fa31faa5
Jenna C <jennac0312@gmail.com>
2023-08-16 13:35:35 -0400

    basic skelly for validaiton route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+const bcrypt = require("bcryptjs") // password encryption
+const jwt = require("jsonwebtoken") // create, sign and send webtokens
+const { check, validationResult } = require("express-validator")
+
+
 // @route:   GET api/users
 // @desc:    Test route
 // @access:  Public
 router.get('/', (req, res) => res.send('User Route'));
+
+// @route:   POST api/users
+// @desc:    Register User
+// @access:  Public
+router.post('/', [] , async ( req, res ) => {
+    // validation happens after route, before callback
+        // array thats hold express validation "middleware"
+
+})
 
 module.exports = router;
